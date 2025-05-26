d200fbf293be5864524335d425c93de5ed0dd46b
sekine kengo <sekinekengo@gmail.com>
2025-05-27 02:46:26 +0900

    ドラッグアンドドロップ機能を持つグリッドの実装を改善しました。色の配列を拡張し、ドラッグ中のアイテムの表示を改善しました。また、コードの可読性を向上させるために変数名を変更しました。

--- drag-and-drop/ContentView.swift
+++ drag-and-drop/ContentView.swift
@@ -8,60 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    /// View properties
-    @State private var colors: [Color] = [.red, .green, .blue, .yellow, .orange, .purple]
-    @State private var draggingItem: Color?
-    var body: some View {
-        NavigationStack{
-            ScrollView(.vertical){
-                let columns = Array(repeating: GridItem(spacing: 10), count: 3)
-                LazyVGrid(columns: columns, spacing: 10, content:{
-                    ForEach(colors,id:\.self){color in
-                        GeometryReader{
-                            let size = $0.size
+  @State private var arrColors: [Color] = [
+    .red, .green, .blue, .yellow, .orange, .purple, .pink, .gray, .black, .white,
+  ]
+  @State private var draggingColor: Color?
+  var body: some View {
+    NavigationStack {
+      ScrollView(.vertical) {
+        let columns = Array(repeating: GridItem(spacing: 10), count: 3)
+        LazyVGrid(
+          columns: columns, spacing: 10,
+          content: {
+            ForEach(arrColors, id: \.self) { color in
+              GeometryReader {
+                let size = $0.size
 
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(color.gradient)
-                                /// Drag
-                                .draggable(color){
-                                    /// custom preview View
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(.ultraThinMaterial)
-                                        .frame(width: 1, height: 1)
-                                        .onAppear{
-                                            draggingItem = color
-                                        }
-                                }
-                                ///Drop
-                                .dropDestination(for: Color.self){items, location in
-                                    draggingItem = nil
-                                    return false
-                                }       isTargeted: {status in
-                                            if let draggingItem, status, draggingItem != color{
-                                                ///Moving Color from source to destination
-                                                if let sourceIndex = colors.firstIndex(of:
-                                                draggingItem),
-                                                    let destinationIndex = colors.firstIndex(of:
-                                                        color){
-                                                withAnimation(.bouncy){
-                                                    let sourceItem = colors.remove(at: sourceIndex)
-                                                    colors.insert(sourceItem, at:
-                                                                    destinationIndex)
-                                                }
-                                            }
-                                    }
-                                }
+                RoundedRectangle(cornerRadius: 10)
+                  .fill(color.gradient)
+                  /// Drag
+                  .draggable(color) {
+                    /// custom preview View
+                    RoundedRectangle(cornerRadius: 10)
+                      .fill(color.gradient.opacity(0.8))
+                      .frame(width: size.width, height: size.height)
+                      .onAppear {
+                        draggingColor = color
+                      }
+                  }
+                  ///Drop
+                  .dropDestination(for: Color.self) { item, location in
+
+                    return false
+                  } isTargeted: { status in
+                    if let draggingColor, status, draggingColor != color {
+                      ///Moving Color from source to destination
+                      if let sourceIndex = arrColors.firstIndex(
+                        of:
+                          draggingColor),
+                        let destinationIndex =
+                          arrColors.firstIndex(of: color)
+                      {
+                        withAnimation(.bouncy) {
+                          let sourceItem = arrColors.remove(at: sourceIndex)
+                          arrColors.insert(
+                            sourceItem,
+                            at:
+                              destinationIndex)
                         }
-                        .frame(height: 100)
+                      }
                     }
-                })
-                .padding(15)
+                  }
+              }
+              .frame(height: 100)
             }
-            .navigationTitle("Movable Grid")
-        }
+          }
+        )
+        .padding(15)
+      }
+      .navigationTitle("Movable Grid")
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
